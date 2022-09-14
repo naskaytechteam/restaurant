166f051ff191fcd49d9ec585de929a49309ee405
nikirana22 <nikirana1983@gmail.com>
2022-09-14 21:24:31 +0530

    refector(home_app_bar) change widgets

--- lib/widgets/homescreen/home_app_bar.dart
+++ lib/widgets/homescreen/home_app_bar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:restaurantapp/widgets/shadow_icon.dart';
+import '/widgets/custom_app_bar.dart';
 import '../../config/colors.dart';
 import '../../screen/cart.dart';
 
@@ -11,54 +11,52 @@ class HomeAppBar extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     double height = size.height;
     double width = size.width;
-    return SizedBox(
-      height: height * 0.1,
-      width: width,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          ShadowIcon(
-            iconClick: () {},
-            isImage: true,
-            imageUrl: 'assets/images/menu.png',
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Location',
-                style: TextStyle(
-                    color: Colors.black54, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                  height: height * 0.03,
-                  width: width * 0.4,
-                  child: Row(
-                    children: const [
-                      Icon(
-                        Icons.location_on,
-                        color: orangeColor,
-                      ),
-                      Text(
-                        'Surakarta, INA',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ))
-            ],
-          ),
-          ShadowIcon(
-              iconClick: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-                  return const Cart();
-                }));
-              },
-              icon: Icons.shopping_cart_outlined),
-        ],
-      ),
+    return CustomAppBar(
+        firstIcon: Image.asset(
+          'assets/images/menu.png',
+          fit: BoxFit.cover,
+        ),
+        centerWidget:locationWidget(height, width) ,
+        secondIcon: const Icon(
+          Icons.shopping_cart_outlined,
+          size: 20,
+          color: Colors.black,
+        ),
+        firstIconClick: () {},
+        secondIconClick: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (_){
+            return const Cart();
+          }));
+        });
+  }
+  Widget locationWidget(double height,double width){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'Location',
+          style:
+          TextStyle(color: Colors.black54, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+            height: height * 0.03,
+            width: width * 0.4,
+            child: Row(
+              children: const [
+                Icon(
+                  Icons.location_on,
+                  color: orangeColor,
+                ),
+                Text(
+                  'Surakarta, INA',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ))
+      ],
     );
   }
 }
